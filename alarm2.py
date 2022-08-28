b78a3df17c8577f74a3f014ef4b82e1e3c2d85e7
Wolfram-180 <sergei.ulvis@gmail.com>
2022-08-28 13:24:42 +0300

    gas alarm

---
+++ alarm2.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+img_rgb = cv2.imread('full_p5.png')
+template = cv2.imread('p5big.png')
+w, h = template.shape[:-1]
+
+res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+threshold = .8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):  # Switch collumns and rows
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+cv2.imwrite('result_yes.png', img_rgb)
+
+img_rgb = cv2.imread('full.png')
+template = cv2.imread('p5big.png')
+w, h = template.shape[:-1]
+
+res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+threshold = .8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):  # Switch collumns and rows
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+cv2.imwrite('result_no.png', img_rgb)
