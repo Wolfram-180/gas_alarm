afdc55c595bd6bd811cfd14866c52f8b01a51f4c
Wolfram-180 <sergei.ulvis@gmail.com>
2022-08-29 09:57:35 +0300

    upd

--- alarm.py
+++ alarm.py
@@ -1,57 +1,40 @@
 from time import sleep
 import cv2
 import numpy as np
+import time
 
-img_rgb = cv2.imread('full_p5.png')
-template = cv2.imread('p5big.png')
-w, h = template.shape[:-1]
-
-res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
-threshold = .8
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):  # Switch collumns and rows
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-cv2.imwrite('result_yes.png', img_rgb)
-
-img_rgb = cv2.imread('full.png')
-template = cv2.imread('p5big.png')
-w, h = template.shape[:-1]
-
-res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
-threshold = .8
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):  # Switch collumns and rows
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-cv2.imwrite('result_no.png', img_rgb)
-
-key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(1)
-sleep(2)
 
 while True:
     try:
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        camimg = timestr + '.png'
+        found = False
         check, frame = webcam.read()
         print(check)  # prints true as long as the webcam is running
         print(frame)  # prints matrix values of each framecd
+
+        sleep(3)
+
         cv2.imshow("Capturing", frame)
+        cv2.imwrite(filename=camimg, img=frame)
+
+        img_rgb = cv2.imread(camimg)
+        template = cv2.imread('p5big.png')
+        w, h = template.shape[:-1]
+
+        res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+        threshold = .8
+        loc = np.where(res >= threshold)
+        for pt in zip(*loc[::-1]):  # Switch collumns and rows
+            found = True
+            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+        if found:
+            cv2.imwrite('result_yes.png', img_rgb)
+
         key = cv2.waitKey(1)
-        if key == ord('s'):
-            cv2.imwrite(filename='saved_img.jpg', img=frame)
-            webcam.release()
-            print("Processing image...")
-            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-            print("Converting RGB image to grayscale...")
-            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-            print("Converted RGB image to grayscale...")
-            print("Resizing image to 28x28 scale...")
-            img_ = cv2.resize(gray, (28, 28))
-            print("Resized...")
-            img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
-            print("Image saved!")
-            break
-        elif key == ord('q'):
+        if key == ord('q'):
             webcam.release()
             cv2.destroyAllWindows()
             break
