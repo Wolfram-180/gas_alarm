673178fbe9d0b37a47fae6ec23c8b190eb5deb2a
Wolfram-180 <sergei.ulvis@gmail.com>
2022-08-30 08:45:57 +0300

    upd

--- alarm.py
+++ alarm.py
@@ -11,27 +11,30 @@ while True:
         camimg = timestr + '.png'
         found = False
         check, frame = webcam.read()
-        print(check)  # prints true as long as the webcam is running
-        print(frame)  # prints matrix values of each framecd
+        print(check)
+        print(frame)
 
-        sleep(3)
+        sleep(1.5)
 
         cv2.imshow("Capturing", frame)
         cv2.imwrite(filename=camimg, img=frame)
 
         img_rgb = cv2.imread(camimg)
-        template = cv2.imread('p5big.png')
+        template = cv2.imread('p5-4.png')
         w, h = template.shape[:-1]
 
         res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
         threshold = .8
         loc = np.where(res >= threshold)
-        for pt in zip(*loc[::-1]):  # Switch collumns and rows
+        for pt in zip(*loc[::-1]):
             found = True
             cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-        if found:
+        if found == True:
             cv2.imwrite('result_yes.png', img_rgb)
+            print('========= FOUND =========')
+            webcam.release()
+            cv2.destroyAllWindows()
 
         key = cv2.waitKey(1)
         if key == ord('q'):
