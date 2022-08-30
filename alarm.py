78f4dffa9eeb5b142e3be24579040891988d33d0
Wolfram-180 <sergei.ulvis@gmail.com>
2022-08-30 17:20:33 +0300

    upd

--- alarm.py
+++ alarm.py
@@ -8,8 +8,10 @@ from safe_bot_token import bot_token
 import requests
 from time import sleep
 
-detection_images = ['5-1.png', '5-2.png', '5-3.png', '5-4.png', 
-                    '6-1.png', '6-2.png', '6-3.png', '6-4.png', ]
+detection_images = ['5-1.png', '5-2.png', '5-3.png', '5-4.png',
+                    '5-5.png', '5-6.png', '5-7.png', '5-8.png',
+                    '6-1.png', '6-2.png', '6-3.png', '6-4.png',
+                    '6-5.png', '6-6.png', '6-7.png', '6-8.png', ]
 cameraIndex = 0  # 0 for laptop webcam, 1 for external webcam
 saving = False  # save the image (True) or not, only show (False)
 looking = True
@@ -18,6 +20,7 @@ tele_message_delay_sec = 3
 match_threshold = 0.8
 init_start_delay_sec = 3.0
 pause_sec_camera = 0.75
+
 
 def webcam_read(webcam):
     check, frame = webcam.read()
